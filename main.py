fbd8b5b54dcb608afea655a046158d64823805a5
tobiolusa <olusamiracle@gmail.com>
2025-07-19 18:37:04 +0100

    add : update and delete route added

--- main.py
+++ main.py
@@ -5,18 +5,46 @@ import schemas
 
 app = FastAPI()
 
+#create blogpost
 @app.post("/blogs", response_model=schemas.BlogResponse)
 async def create_blog(blog: schemas.BlogCreate):
     blog_obj = await Blog.create(**blog.dict())
     return await schemas.BlogResponse.from_tortoise_orm(blog_obj)
 
+#get all blog posts
 @app.get("/blogs", response_model=list[schemas.BlogResponse])
 async def get_blogs():
     blogs = await Blog.all()
     return [await schemas.BlogResponse.from_tortoise_orm(blog) for blog in blogs]
 
+#get a single post by id
 @app.get("blogs/{blog_id}", response_model=schemas.BlogResponse)
 async def get_blog(blog_id:int):
     blog = await Blog.get_or_none(id=blog_id)
     if not blog:
         raise HTTPException(status_code=404, detail="Blog not found")
+    return await schemas.BlogResponse.from_tortoise_orm(blog)
+
+#update a post 
+@app.get("blogs/{blog_id}", response_model=schemas.BlogResponse)
+async def update_blog(blog_id:int):
+    blog_obj = await Blog.get_or_none(id=blog_id)
+    if not blog_obj:
+        raise HTTPException(status_code=404, detail="Not found")
+    
+    blog_data = blog.dict(exclude_unset=True)
+    for field, value in blog_data.items():
+        setattr(blog_obj, field, value)
+
+    await blog_obj.save()
+    return await schemas.BlogResponse.from_tortoise_orm(blog_obj)
+
+#Delete blog
+@app.delete("blogs/{blog_id}")
+async def delete_blog(blog_id:int):
+    blog = await Blog.get_or_none(id=blog_id)
+    if not blog:
+        raise HTTPException(status_code=404, detail="Not found")
+    await blog.delete()
+    return {"message" : "Blog deleted successfully"}
+
